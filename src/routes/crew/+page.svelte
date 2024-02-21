8575a56f916fb4282ac5d0685c2eb7edc1bd1749
Emona Santiago <emonasantiago1207@gmail.com>
2024-02-21 16:09:33 +0100

    pages route fixed

---
+++ src/routes/crew/+page.svelte
@@ -0,0 +1,71 @@
+<script>
+    import Navigation from "../../lib/components/3navigation.svelte";
+</script>
+
+<body>
+<Navigation></Navigation>
+<div class="row">
+    <div class="column">
+        <h1>IMPACT</h1>
+    </div>
+    <div class="column">
+      <h2>Crew page</h2>
+    </div>
+  </div>
+</body>
+<style>
+    * {
+        font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+    }
+
+    :root {
+        --impact-yellow: #F8F053;
+    }
+
+    body {
+    margin: 0;
+    background-image: url("creative.png");
+    height: 100vh;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    text-align: center;
+    }
+
+    * {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+h1 {
+    color: var(--impact-yellow);
+    font-size: 120px;
+}
+
+h2 {
+  color: white;
+}
+
+
+/* Create two equal columns that floats next to each other */
+.column {
+  float: left;
+  width: 50%;
+  padding: 10px;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Responsive layout - makes the two columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 600px) {
+  .column {
+    width: 100%;
+  }
+}
+</style>
